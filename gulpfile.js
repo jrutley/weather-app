997dd7c43e8efc5167404aa1a8f90d53710576f7
Jeramy Rutley <jrutley@gmail.com>
2017-01-26 23:49:04 -0500

    Changed to livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,29 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var nodemon = require('gulp-nodemon');
-
-/**
- * Gulp Tasks
- */
-
-gulp.task('browser-sync', ['nodemon'], function() {
-  browserSync({
-    proxy: "localhost:3000",  // local node app address
-    port: 5000,  // use *different* port than above
-    notify: true
+var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass'),
+  watch = require('gulp-watch');
+ 
+gulp.task('webserver', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
   });
 });
 
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
-    script: 'app.js',
-    ignore: [
-      'gulpfile.js',
-      'node_modules/'
-    ]
-  })
-  .on('start', function () {
-    if (!called) {
-      called = true;
-      cb();
-    }
-  })
-  .on('restart', function () {
-    setTimeout(function () {
-      reload({ stream: false });
-    }, 1000);
-  });
+gulp.task('sass', function() {
+  gulp.src('app/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['browser-sync'], function () {
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+gulp.task('reload', function(){
+  gulp.src('app/*.html').pipe(connect.reload());
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['sass/**/*.scss'], {cwd: 'app'}, ['sass']);
+    gulp.watch(['*.html', 'scripts/**/*.js'], {cwd: 'app'}, ['reload']);
 });
+ 
+gulp.task('default', ['sass', 'webserver', 'watch']);
